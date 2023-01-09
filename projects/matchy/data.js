0a6e2a5317f5e93313ad452d197a2f9df9323354
Bryan Burnside <bpburnside@gmail.com>
2023-01-09 22:03:22 +0000

    Completed pt 1 of matchy.js

--- projects/matchy/data.js
+++ projects/matchy/data.js
@@ -11,28 +11,35 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
-
+var animal = {};
+animal.species = 'cat';
+animal['name'] = 'Fofi';
+animal.noises = [];
+console.log(animal);
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
-
+var noises = [];
+noises[0] = 'purr';
+noises.push('hiss');
+noises.unshift('meow');
+noises[noises.length] = 'sneeze';
+console.log(noises.length);
+console.log(noises[noises.length - 1]);
+console.log(noises);
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
-
+animal['noises'] = noises;
+animal.noises.push('cough');
+console.log(animal);
 /* *******************************************************************
  * Step 4 - Review
  *
  * 1. What are the different ways you can access properties on objects?
- *
+ *    dot / bracket notation
  * 2. What are the different ways of accessing elements on arrays?
- *
+ *    bracket notation / .length
  * *******************************************************************
  */
 
@@ -48,15 +55,38 @@
 //////////////////////////////////////////////////////////////////////
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
-
+var animals = [];
+animals.push(animal);
+console.log(animals);
+var duck = { species: 'duck', name: 'Jerome', noises: ['quack', 'honk', 'sneeze', 'woosh'] };
+animals.push(duck);
+console.log(animals);
+var dog = {
+  species: 'dog',
+  name: 'Hecker',
+  noises: ['woof', 'bark']
+};
+animals.push(dog);
+var rat = {
+  species: 'rat',
+  name: 'Splinter',
+  noises: ['mysticism', 'addages']
+};
+animals.push(rat);
+console.log(animals);
+console.log(animals.length);
 //////////////////////////////////////////////////////////////////////
 // Step 7 - Making Friends ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
-
+var friends = []; // we chose an array, since it is a list
+function getRandom(animals) {
+    var min = 0;
+    var max = animals.length - 1;
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+friends.push(animals[getRandom(animals)].name);
+console.log(friends);
+animals[0]['friends'] = friends;
 /**
  * Nice work! You're done Part 1. Pat yourself on the back and
  * move onto Part 2 in the file called "functions.js"
