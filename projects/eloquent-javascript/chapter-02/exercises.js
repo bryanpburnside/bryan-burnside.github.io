456c701f76975201de7a4ecc344d1e0a98f678a2
Bryan Burnside <bpburnside@gmail.com>
2023-01-12 19:50:33 +0000

    completed eloquent javascript ch 2

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -32,18 +32,22 @@ function fizzBuzz(start, end) { // function takes a start param and an end param
 ////////////////////////////////////////////////////////////////////////////////
 
 function drawChessboard(rows) {
-  let combined = ''
-  let oddPos = ' ';
-  let evenPos = '#';
-  for(let i = 1; i <= rows; i++) {
-    for(let j = 1; j <= rows; j++) {
-      if((i + j) % 2 === 0) {
-        console.log(combined += oddPos + evenPos);
-      } else if(j === rows) {
-        console.log(combined + "\n");
+  let combined = '' // the combined odd/even characters to form the chessboard
+  let evenPos = ' '; // starting character if the row/char pos is even (i.e. pos [1, 1])
+  let oddPos = '#'; // starting character if the row/char pos is odd (i.e. pos [1, 2])
+  for(let i = 1; i <= rows; i++) { // loop to handle rows
+    for(let j = 1; j <= rows; j++) { // loop to handle characters
+      if((i + j) % 2 === 0) { // if the position is an even position
+        combined += evenPos; // add a space to our chessboard
+      } else if((i + j) % 2 !== 0) { // if the position is an odd position
+        combined += oddPos; // add a hash to our chessboard
+      }
+      if(j === rows) { // if our character loop is at the end of the line
+        combined += '\n'; // add a line break
       }
     }
   }
+  console.log(combined); // print our chessboard to the console
 }
 
 ////////////////////////////////////////////////////////////////////////////////
