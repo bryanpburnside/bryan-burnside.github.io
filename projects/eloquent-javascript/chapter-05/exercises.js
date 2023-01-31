e45344a273d641af9e2cb00ab627a1b1b1f3162b
Bryan Burnside <bpburnside@gmail.com>
2023-01-31 20:10:51 +0000

    completed EJ CH5

--- projects/eloquent-javascript/chapter-05/exercises.js
+++ projects/eloquent-javascript/chapter-05/exercises.js
@@ -25,20 +25,28 @@ function loop(value, test, update, body) {
 // /////////////////////////////////////////////////////////////////////////////
 
 function every(array, test) {
-  for(let i = 0; i < array.length; i++) {
-    if (!test(array[i])) {
-      return false;
+  for(let i = 0; i < array.length; i++) { // loop through the array
+    if (!test(array[i])) { // if the test on an element in the array resolves to false
+      return false; // then return false
     }
   }
-  return true;
+  return true; // otherwise, all elements passed the test so return true
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // dominantDirection ///////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function dominantDirection() {
-  
+function dominantDirection(string) {
+  let ltr = 0; // create variable to keep track of ltr scripts
+  let rtl = 0; // create variable to keep track of rtl scripts
+  for(let i = 0; i < string.length; i++) { // loop through the string
+    let script = characterScript(string.charCodeAt(i)); // let script = object | null
+    if(script !== null) { // if the script does not equal null
+      script.direction === 'ltr' ? ltr++ : rtl++; // check the value of its direction property equals ltr, if so add one, otherwise add one to rtl
+    }
+  }
+  return ltr > rtl ? 'ltr' : 'rtl'; // if ltr is greater than rtl, return 'ltr', otherwise return 'rtl'
 }
 
 // /////////////////////////////////////////////////////////////////////////////
